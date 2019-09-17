9e9bb7992d80dfb1aec3dfea993f0a9aafac5e4f
Pierre Bretéché <pbreteche@dawan.fr>
2019-09-17 10:53:47 +0200

    création de l'entité Publication

---
+++ src/Entity/Publication.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PublicationRepository")
+ */
+class Publication
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $publieeLe;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $etat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getPublieeLe(): ?\DateTimeInterface
+    {
+        return $this->publieeLe;
+    }
+
+    public function setPublieeLe(?\DateTimeInterface $publieeLe): self
+    {
+        $this->publieeLe = $publieeLe;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+}
